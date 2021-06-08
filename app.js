619d085fd4c3ffd2e37b4dddc8102816146144cf
Rupesh Budhathoki <54392112+Roopaish@users.noreply.github.com>
2021-06-08 22:33:40 +0545

    project showcase site

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
-const section = document.getElementById('projects')
+const section = document.getElementById("projects")
 
+let projects = [
+  { id: 1, tag: "Easy", title: "Transparent Login Form", img: "tlf" },
+  { id: 1, tag: "", title: "", img: "" },
+]
+
+projects.map((project) => {
+  section.innerHTML += `
+    <article>
+    <div class='tag'>${project.tag}</div>
+
+    <div class='title'>
+      <a href="/${project.title}">${project.title}</a>
+    </div>
+
+    <figure>
+      <img src='img/${project.img}.png' alt = ${project.title}>
+    </figure>
+
+    <a href="https://github.com/Roopaish/CSS-RoadMap/tree/master/${project.title}" class='code-link'>
+      View Project Code →
+    </a>
+
+  </article>
+`
+})
