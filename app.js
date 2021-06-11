579847c501af21f858172fbf6abd45f28f8dfbf1
Rupesh Budhathoki <54392112+Roopaish@users.noreply.github.com>
2021-06-11 20:24:24 +0545

    😀😷🌟CSS Only Emoji Art

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let projects = [
   { tag: "Easy", title: "Transparent Login Form", img: "tlf" },
   { tag: "Easy", title: "Responsive Video Background", img: "rvb" },
   { tag: "Medium", title: "CSS Only Tooltips", img: "ctt" },
+  { tag: "Medium", title: "CSS Only Emoji Art", img: "coea" },
 ]
 
 projects.map((project) => {
